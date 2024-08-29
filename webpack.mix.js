46a2bc6fdedb9c7fd56cd055dbbba88f763b0246
Bruno / <49591443+bruno-carneiro-da-silva@users.noreply.github.com>
2024-08-29 18:42:27 -0300

    :construction: feat: setting up js files with mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,4 +2,5 @@ const mix = require("laravel-mix");
 
 mix.js("resources/js/autocomplete.js", "public/js")
     .js("resources/js/notifications.js", "public/js")
+    .js("resources/js/chatbot.js", "public/js")
     .sass("resources/sass/app.scss", "public/css");
